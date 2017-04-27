8d15183836eec6d3751f129901d82d416ad4067c
Jesper Orb <jesper.orb@zocom.se>
2017-04-27 17:33:01 +0200

    Initialize repository

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect-php');
+const browserSync = require('browser-sync');
+
+gulp.task('default', () => {
+  connect.server({}, () => {
+    browserSync({
+      proxy: '127.0.0.1:8000',
+      notify: {
+        styles: {
+          top: 'auto',
+          bottom: '0',
+          opacity: '1'
+        }
+      }
+    });
+  });
+  gulp.watch('*.php').on('change', () => {
+    browserSync.reload();
+  });
+});
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php  include 'includes/head.php'; ?>
+
+<h1>Hey!</h1>
+
+<?php include 'includes/footer.php'; ?>
